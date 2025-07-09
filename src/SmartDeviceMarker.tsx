c124280131886c604807e0cd66d2b9f25ebcfdf0
Brandon Jong <bjongis@gmail.com>
2025-07-09 09:35:27 -0700

    Added tooltips and css table formatting

--- src/SmartDeviceMarker.tsx
+++ src/SmartDeviceMarker.tsx
@@ -15,11 +15,33 @@ export class SmartDeviceMarker extends Marker {
     // Call base class constructor
     super(location, size);
 
-    // Move all the code from the challenge into the new class
-    this.title = smartDeviceId;
     this.setImageUrl(`${smartDeviceType}.svg`);
+    this.title = this.populateTitle(smartDeviceId, federatedData);
     //this.label = smartDeviceType;
     //this.labelOffset = { x: 0, y: 30 };
     //this.labelColor = "black"; // ✅ Add this line to set label text color    
+  }
+
+  private populateTitle(smartDeviceId: string, federatedData: DeviceData) {
+    // Iterate through the cloud data object to create a row for each data entry 
+    let smartTable = "";
+    for (const [key, value] of Object.entries(federatedData)) {
+      smartTable += `
+        <tr>
+            <td>${key}</td>
+            <td>${value}</td>
+        </tr>
+      `;
+    };
+
+    const smartTableDiv = document.createElement("div");
+    smartTableDiv.className = "smart-table"; // Now our table will look awesome!
+    smartTableDiv.innerHTML = `
+      <h3>${smartDeviceId}</h3>
+      <table>
+      ${smartTable}
+      </table>
+    `;
+    return smartTableDiv;
   }
 }
